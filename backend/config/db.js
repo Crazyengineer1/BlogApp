1b1a125fc7d834df2525640a7e5a1b5330643303
Mayank Kumar <mayankkumar82488@gmail.com>
2025-04-26 19:32:54 +0530

    doing some changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,6 @@ const connectDB = async () => {
     const URI = process.env.MONGO_URI;
     try {
         const conn = await mongoose.connect(URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
             serverSelectionTimeoutMS: 30000, // Wait up to 30 seconds for server selection
             socketTimeoutMS: 45000,           // Wait up to 45 seconds for socket operations
         });
